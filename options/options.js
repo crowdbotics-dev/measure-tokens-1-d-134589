8d7876be8b74b9fdb8a402f8f8a43d1b3f0627d5
Crowdbotics <engineering@crowdbotics.com>
2024-03-19 17:31:13 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "measure-tokens-1-d-134589",
+  url: "https://measure-tokens-1-d-134589.botics.co",
+  api: "https://measure-tokens-1-d-134589.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
